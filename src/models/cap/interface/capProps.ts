5b5e67a3c7d87bc68e801e3eb08b0bff10801900
fvknk97034 <97640891+fvknk97034@users.noreply.github.com>
2026-06-21 23:09:44 +0000

    不要なファイルを削除

--- src/models/cap/interface/capProps.ts
+++
@@ -1,7 +0,0 @@
-export interface CapProps {
-  labels: string[];
-  width?: number;
-  height?: number;
-  colStart?: number | null;
-  fixed?: boolean
-}
--- src/models/cap/interface/grid.ts
+++
@@ -1,4 +0,0 @@
-export interface Grid {
-  gridColumn?: string;
-  gridRow?: string;
-}
